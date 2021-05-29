3d885abc52478d602bb6b05ac568d9a044072b0a
Brandon Benoit <b.nutty@gmail.com>
2021-05-29 09:09:54 -0700

    added put and delete routes to campaign routes

--- src/routes/campaignRoutes.js
+++ src/routes/campaignRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const ObjectID = require('mongoose').Types.ObjectId;
 const requireAuth = require('../middleware/requireAuth');
 
 const Campaign = mongoose.model('Campaign');
@@ -30,8 +31,38 @@ router.post('/campaign', async (req, res) => {
         await campaign.save();
         res.send(campaign);
     } catch (err) {
-        res.status.send({ error: err.message });
+        res.status(400).send({ error: err.message });
     }
 });
+
+router.put('/campaign/:id', async (req, res) => {
+    const { monster } = req.body;
+
+    if(!ObjectID.isValid(req.params.id)) {
+        return res.status(400).send('No campaign with the id:' + req.params.id)
+    }
+
+    try {
+        let campaign = await Campaign.findById(req.params.id);
+        campaign.monsters.push(monster);
+        await Campaign.updateOne({ _id: req.params.id }, { monsters: campaign.monsters });
+        const myCampaigns = await Campaign.find({ userId: req.user._id });
+        res.send(myCampaigns);
+    } catch (err) {
+        res.status(400).send({ error: err.message });
+    }
+})
+
+router.delete('/campaign/:id', async (req, res) => {
+    if(!ObjectID.isValid(req.params.id)) {
+        return res.status(400).send('No campaign with the id:' + req.params.id)
+    }
+    
+    try { 
+        await Campaign.findByIdAndRemove(req.params.id);
+    } catch (err) {
+        res.status(400).send({ error: err.message });
+    }
+})
 
 module.exports = router;
